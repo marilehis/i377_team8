41abe1bb3677ea642ac89dded576cf70306c8ebb
Tanel Lebedev <tanel.lebedev@gmail.com>
2011-12-04 11:17:13 +0200

    Piirilõigu kustutamine korda

--- src/main/java/ee/itcollege/i377/web/PiiriloikController.java
+++ src/main/java/ee/itcollege/i377/web/PiiriloikController.java
@@ -1,8 +1,5 @@
 package ee.itcollege.i377.web;
 
-import ee.itcollege.i377.entities.Piiriloik;
-
-
 import java.security.Principal;
 import java.util.Date;
 
@@ -18,59 +15,58 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import ee.itcollege.i377.entities.Piiriloik;
 
 @RooWebScaffold(path = "piiriloiks", formBackingObject = Piiriloik.class)
 @RequestMapping("/piiriloiks")
 @Controller
 public class PiiriloikController {
-	
-	private Piiriloik setCreated(Piiriloik piiriloik,Principal principal){
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String create(@Valid Piiriloik piiriloik,
+			BindingResult bindingResult, Principal principal, Model uiModel,
+			HttpServletRequest httpServletRequest) {
+		if (bindingResult.hasErrors()) {
+			uiModel.addAttribute("piiriloik", piiriloik);
+			addDateTimeFormatPatterns(uiModel);
+			return "piiriloiks/create";
+		}
+		uiModel.asMap().clear();
 		piiriloik.setAvaja(principal.getName());
 		piiriloik.setAvatud(new Date());
 		piiriloik.persist();
-        return piiriloik;
+
+		return "redirect:/piiriloiks";
 	}
-	
-	
-	private Piiriloik setModified(Piiriloik piiriloik,Principal principal){
-		piiriloik.setMuutja(principal.getName());
+
+	@RequestMapping(method = RequestMethod.PUT)
+	public String update(@Valid Piiriloik piiriloik,
+			BindingResult bindingResult, Principal principal, Model uiModel,
+			HttpServletRequest httpServletRequest) {
+		if (bindingResult.hasErrors()) {
+			uiModel.addAttribute("piiripunkt", piiriloik);
+			addDateTimeFormatPatterns(uiModel);
+			return "piiriloiks/update";
+		}
+		uiModel.asMap().clear();
 		piiriloik.setMuudetud(new Date());
+		piiriloik.setMuutja(principal.getName());
 		piiriloik.merge();
-        return piiriloik;
+		return "redirect:/piiriloiks";
 	}
-	
-	private String hasErrors(Piiriloik piiriloik,Model uiModel,String go_to){
-		uiModel.addAttribute("piiriloik", piiriloik);
-        addDateTimeFormatPatterns(uiModel);
-		return go_to;
+
+	@RequestMapping(value = "/{piiriloikId}", method = RequestMethod.DELETE)
+	public String delete(@PathVariable("piiriloikId") Long piiriloikId,
+			@RequestParam(value = "page", required = false) Integer page,
+			@RequestParam(value = "size", required = false) Integer size,
+			Principal principal, Model uiModel) {
+		Piiriloik piiriloik = Piiriloik.findPiiriloik(piiriloikId);
+		piiriloik.setSuletud(new Date());
+		piiriloik.setSulgeja(principal.getName());
+		piiriloik.merge();
+		uiModel.asMap().clear();
+		uiModel.addAttribute("page", (page == null) ? "1" : page.toString());
+		uiModel.addAttribute("size", (size == null) ? "10" : size.toString());
+		return "redirect:/piiriloiks";
 	}
-	
-	
-	 @RequestMapping(method = RequestMethod.POST)
-	 public String create(@Valid Piiriloik piiriloik, BindingResult bindingResult, Principal principal, Model uiModel, HttpServletRequest httpServletRequest) {
-		if (bindingResult.hasErrors())return this.hasErrors(piiriloik, uiModel, "piiriloik/create");
-        uiModel.asMap().clear();
-        piiriloik = this.setCreated(piiriloik,principal);     
-        return "redirect:/piiriloiks";
-	 }
-	    	
-    @RequestMapping(method = RequestMethod.PUT)
-    public String update(@Valid Piiriloik piiriloik, BindingResult bindingResult, Principal principal, Model uiModel, HttpServletRequest httpServletRequest) {
-        if (bindingResult.hasErrors()) this.hasErrors(piiriloik, uiModel, "piiriloiks/update");
-        uiModel.asMap().clear();
-        piiriloik = this.setModified(piiriloik,principal);
-        piiriloik.merge();
-        return "redirect:/piiriloiks";
-    }
-    
-    @RequestMapping(value = "/{piiriloikId}", method = RequestMethod.DELETE)
-    public String delete(@PathVariable("piiriloikId") Long piiriloikId, @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, Principal principal, Model uiModel) {
-    	Piiriloik piiriloik = Piiriloik.findPiiriloik(piiriloikId);
-    	piiriloik = this.setModified(piiriloik,principal);
-    	piiriloik.merge();
-        uiModel.asMap().clear();
-        uiModel.addAttribute("page", (page == null) ? "1" : page.toString());
-        uiModel.addAttribute("size", (size == null) ? "10" : size.toString());
-        return "redirect:/piiriloiks";
-    }
 }
